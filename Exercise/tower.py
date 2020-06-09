a3ddbe24fc172764f974d0bfe8fe95f23a58f7dc
JungRyung <37282346+JungRyung@users.noreply.github.com>
2020-06-09 17:33:43 +0900

    programmers

--- Exercise/tower.py
+++ Exercise/tower.py
@@ -1,16 +1,28 @@
+# def solution(heights):
+# 	answer = []
+# 	idx = 0
+# 	tallest = 0
+# 	num = 1
+# 	for i in heights:
+# 		if tallest >= i:
+# 			answer.append(idx)
+# 		else:
+# 			answer.append(0)
+# 			tallest = i
+# 			idx = num
+# 		num+=1
+# 	return answer
+
 def solution(heights):
 	answer = []
-	idx = 0
-	tallest = 0
-	num = 1
-	for i in heights:
-		if tallest >= i:
-			answer.append(idx)
-		else:
-			answer.append(0)
-			tallest = i
-			idx = num
-		num+=1
+	tower_stack = Stack()
+	temp_stack = Stack()
+	tower_stack.push(heights[0])
+	answer.append(0)
+	for idx in range(1:len(tower_stack)):
+		while tower_stack.is_empty() != True:
+			
+
 	return answer
 
 # Stack 클래스 정의 - python list 활용
@@ -23,7 +35,7 @@ class Stack(list):
         else:
             return False
 
-    def peek(self):        # 최상단 데이터 확인
+    def top(self):        # 최상단 데이터 확인
         return self[-1]
 
 heights = [6,9,5,7,4]
