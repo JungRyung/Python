782088175ec1bd44b8238885f5772601b3f3ef46
JungRyung <soondori92@gmail.com>
2021-05-25 16:29:05 +0900

    update coding test

---
+++ Exercise/dust_cleaning.py
@@ -0,0 +1,15 @@
+########## 2021 상반기 조폐공사 코딩테스트 3번문제 ###########
+def solution(area):
+    answer = 0
+    height = len(area)
+    width = len(area[0])
+    
+    for i in range(height):
+        for j in range(width):
+            
+
+    return answer
+
+area = [".$...$..","......$.","$....###","..$.....","...$....","..$...$.","########"]
+answer =  solution(area)
+print(answer)
